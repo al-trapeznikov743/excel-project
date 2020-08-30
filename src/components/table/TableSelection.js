201cbd34d0e13465d0f65a6f69d068289b903e97
al-trapeznikov <al.trapeznikov.743@gmail.com>
2020-08-30 23:22:48 +0500

    finish base table logic

---
+++ src/components/table/TableSelection.js
@@ -0,0 +1,23 @@
+export class TableSelection {
+    static className = 'selected'
+    constructor() {
+        this.group = []
+        this.current = null
+    }
+
+    select($el) {
+        this.clear()
+        this.group = []
+        this.group.push($el)
+        $el.focus().addClass(TableSelection.className)
+        this.current = $el
+    }
+    selectGroup($group = []) {
+        this.clear()
+        this.group = $group
+        this.group.forEach($el => $el.addClass(TableSelection.className))
+    }
+    clear() {
+        this.group.forEach($cell => $cell.removeClass(TableSelection.className))
+    }
+}
